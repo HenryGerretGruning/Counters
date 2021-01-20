d7d7193b5ce4f423088bb14608a188b6505be7f1
Henry-Gerret Grüning <artimax32@gmail.com>
2021-01-20 12:42:19 +0200

    Restarted

--- counters/src/index.js
+++ counters/src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ 
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+ 
+const Counter = (props) => {
+ // const [counter, setCounter] = ... ; // muutuja ja muutuja “setter”
+ // const updateCounter = () => { ... } // funktsioon
+ // return ( ... ) // JSX
+}
+ 
+const App = () => {
+return (
+  <main>
+    <h1>Counters</h1>
+    <Counter step={1}></Counter>
+    <Counter step={2}></Counter>
+    <Counter step={0.1}></Counter>
+  </main>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+};
+ 
+ReactDOM.render(<App />, document.getElementById("root"));
