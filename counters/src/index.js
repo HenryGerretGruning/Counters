9cb1fb81c923f70c4de4ef13623550f86c22af82
Henry-Gerret Grüning <artimax32@gmail.com>
2021-01-20 21:42:23 +0200

    Made counter functonal

--- counters/src/index.js
+++ counters/src/index.js
@@ -1,18 +1,32 @@
- 
-import React from "react";
+import React, { useState } from 'react';
 import ReactDOM from "react-dom";
 import "./index.css";
  
 const Counter = (props) => {
- // const [counter, setCounter] = ... ; // muutuja ja muutuja “setter”
- // const updateCounter = () => { ... } // funktsioon
- // return ( ... ) // JSX
+  const [counter, setCounter] = useState(0) ; // muutuja ja muutuja “setter”
+  const updateCounter = () => { 
+    
+   } // funktsioon
+  return ( 
+
+    
+    <div class ="step">
+      <h1>step: {props.step}</h1>
+      <div class= "box">
+        <button  onClick={() => setCounter(counter - props.step)}>-</button>
+        <h1> {counter} </h1>
+      <button  onClick={() => setCounter(counter + props.step)}>+</button>
+      </div>
+      
+    </div>
+    
+    ) // JSX
 }
  
 const App = () => {
 return (
   <main>
-    <h1>Counters</h1>
+    <h1 class="title">Counters</h1>
     <Counter step={1}></Counter>
     <Counter step={2}></Counter>
     <Counter step={0.1}></Counter>
