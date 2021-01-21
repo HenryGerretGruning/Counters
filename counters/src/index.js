84ad637e32b5cba15ef8e5594dcbaa52561ed0fd
Henry-Gerret Grüning <artimax32@gmail.com>
2021-01-21 14:02:05 +0200

    Finished

--- counters/src/index.js
+++ counters/src/index.js
@@ -2,15 +2,24 @@ import React, { useState } from 'react';
 import ReactDOM from "react-dom";
 import "./index.css";
  
-const Counter = (props) => {
+const Counter = (props,sign) => {
   const [counter, setCounter] = useState(0) ; // muutuja ja muutuja “setter”
   const updateCounter = () => { 
-    if (counter <0){
-      console.log("Limit reached")
+    if (sign == "+") {
+      if (counter >=10){
+        console.log("Limit reached")
+      } else if ( counter <10) {
+        setCounter(counter + props.step)
+      }
+
+    } else if (sign == "-") {
+      if (counter <=0){
+        console.log("Limit reached")
+      } else if ( counter > 0) {
+        setCounter(counter - props.step)
+      }
+    }
     
-  } else {
-    setCounter(counter - props.step)
-  }
    } // funktsioon
   return ( 
 
@@ -18,9 +27,9 @@ const Counter = (props) => {
     <div class ="step">
       <h1>step: {props.step}</h1>
       <div class= "box">
-        <button  onClick={() => updateCounter("-")}>-</button>
+        <button  onClick={() => updateCounter(sign= ["-"] )}>-</button>
         <h1> {counter} </h1>
-      <button  onClick={() => updateCounter("+")}>+</button>
+      <button  onClick={() => updateCounter(sign= ["+"])}>+</button>
       </div>
       
     </div>
